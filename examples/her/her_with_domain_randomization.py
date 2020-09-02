01e974ed4cbc7b0062b3a5d7b58130a0e4be0c64
Karanbir Chahal <karanbleep@gmail.com>
2020-09-01 20:49:10 -0400

    Added viskit dep and some changes

---
+++ examples/her/her_with_domain_randomization.py
@@ -0,0 +1,12 @@
+import gym
+
+import rlkit.torch.pytorch_util as ptu
+from rlkit.data_management.obs_dict_replay_buffer import ObsDictRelabelingBuffer
+from rlkit.launchers.launcher_util import setup_logger
+from rlkit.samplers.data_collector import GoalConditionedPathCollector
+from rlkit.torch.her.her import HERTrainer
+from rlkit.torch.networks import ConcatMlp
+from rlkit.torch.sac.policies import MakeDeterministic, TanhGaussianPolicy
+from rlkit.torch.sac.sac import SACTrainer
+from rlkit.torch.torch_rl_algorithm import TorchBatchRLAlgorithm
+
--- examples/sac.py
+++ examples/sac.py
@@ -1,5 +1,7 @@
 from gym.envs.mujoco import HalfCheetahEnv
+import gym 
 
+# env = gym.make('Pendulum-v0')
 import rlkit.torch.pytorch_util as ptu
 from rlkit.data_management.env_replay_buffer import EnvReplayBuffer
 from rlkit.envs.wrappers import NormalizedBoxEnv
@@ -12,8 +14,8 @@ from rlkit.torch.torch_rl_algorithm import TorchBatchRLAlgorithm
 
 
 def experiment(variant):
-    expl_env = NormalizedBoxEnv(HalfCheetahEnv())
-    eval_env = NormalizedBoxEnv(HalfCheetahEnv())
+    expl_env = NormalizedBoxEnv(env)
+    eval_env = NormalizedBoxEnv(env)
     obs_dim = expl_env.observation_space.low.size
     action_dim = eval_env.action_space.low.size
 
